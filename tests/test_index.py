2a16c846a2f1890bb780cc50fc175e45769aa5b8
Cristal Q <hilaryc.quirinop2010@gmail.com>
2026-06-16 20:50:06 -0600

    Se corrige la prueba del index

--- tests/test_index.py
+++ tests/test_index.py
@@ -2,5 +2,4 @@ def test_titulo():
     with open("index.html", "r", encoding="utf-8") as archivo:
         contenido = archivo.read()
 
-    assert "Hola Mundo" in contenido
-    
+    assert "Bienvenido" in contenido
